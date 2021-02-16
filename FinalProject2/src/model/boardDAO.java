34e849efcdbdecbba3dd3bc81c97768ddf249c10
Chosun <Chosun@DESKTOP-LFSHGNF>
2021-02-16 11:01:00 +0900

    ddd

---
+++ FinalProject2/src/model/boardDAO.java
@@ -0,0 +1,45 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class boardDAO {
+	int cnt =0;
+	PreparedStatement psmt = null;
+	Connection conn = null;
+	ResultSet rs = null;
+	customersVO userInfo = null;
+	
+	public void conn() {
+		try {
+				Class.forName("oracle.jdbc.driver.OracleDriver");
+				String url = "jdbc:oracle:thin:@localhost:1521:xe";
+				String dbid = "hr";
+				String dbpw = "hr";
+				
+				conn = DriverManager.getConnection(url,dbid,dbpw);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public void close() {
+		try {
+			if(rs!=null) {
+				rs.close();
+			}
+			if(psmt != null) {
+					psmt.close();
+			}if(conn != null) {
+				conn.close();
+			}
+			
+			}catch (Exception e) {
+					e.printStackTrace();
+				}
+	}
+	
+	
+
+}
